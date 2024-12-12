005b2d6313f19b159a9a3329cefc44e75a1789cd
Gab-98 <19975253+Gab-98@users.noreply.github.com>
2024-12-12 18:41:52 -0300

    Style: Change the position of some lines of code
    Put the randomNumber variable in the getComputerChoice function scope instead of the global scope
    
    Put console.log(getComputerChoice()) before the console.log(getHumanChoice()) so it appears in the browser console first and the human choice after it

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let randomNumber = Math.floor(Math.random() * 3) + 1
-
 function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3) + 1
     const computerChoice = 'Computer played: '
 
     if (randomNumber === 1) {
@@ -13,13 +12,13 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt('What will you play?')
+    let humanChoice = prompt('What will you play? (Only write rock, paper or scissor. Also, write in lower case only!)')
 
     while (humanChoice !== 'rock' && humanChoice !== 'paper' && humanChoice !== 'scissor'){
-        humanChoice = prompt('Only write rock, paper or scissor. Also, write in lower case only!')
+        humanChoice = prompt('Invalid input')
     }
 
     return 'Human played: ' + humanChoice.toUpperCase()
 }
-console.log(getHumanChoice())
 console.log(getComputerChoice())
+console.log(getHumanChoice())
