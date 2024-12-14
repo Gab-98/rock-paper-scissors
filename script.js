6e7214091d928459b796b42ca5cd95dc2ec971e2
Gab-98 <19975253+Gab-98@users.noreply.github.com>
2024-12-13 21:08:13 -0300

    Change the whole code to something simpler
    No global scope variables
    Only one function with 3 variables in it
    
    'For' loop inside the function repeating for 5 times (or 5 rounds ), to check each win condition between the human and the computer
    
    Computer choice and human choice code is also inside the 'for' loop, to start each round with new values
    
    Check if the human input is valid or not will happen if the input is not rock, paper or scissors, letter case doesn't matter because these words will be converted into all lowercase, this check only happens if the input value is a number, a different word or the spelling is wrong
    
    It will ask the user to refresh the page if an invalid input was inserted
    
    The loop breaks when either player reaches 3 points, otherwise the game will continue for a total of 5 rounds
    
    The winner will the one with more points, otherwise it will be considered a tie

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3) + 1
-    const computerChoice = 'Computer played: '
+function playGame () {
+    let humanScore = 0
+    let computerScore = 0
 
-    if (randomNumber === 1) {
-        return computerChoice + 'ROCK' 
-    } else if (randomNumber === 2) {
-        return computerChoice + 'PAPER'
-    } else if (randomNumber === 3) {
-        return computerChoice + 'SCISSOR'
+    for (let i = 0; i < 5; i++) {
+        const choices = ["rock", "paper", "scissors"]
+        const randIndex = Math.floor(Math.random() * choices.length)
+        const computerChoice = choices[randIndex].toLowerCase()
+        const humanChoice = prompt("Type rock, paper or scissors").toLowerCase()
+
+        console.log(`Human played: ${humanChoice}`) 
+        console.log(`Computer played: ${computerChoice}`)
+
+        if (humanChoice === computerChoice) {
+            console.log("It's a draw!");
+        } else if (humanChoice === 'rock' && computerChoice === 'scissors') {                       
+            humanScore++
+        } else if (humanChoice === 'rock' && computerChoice === 'paper') {            
+            computerScore++
+        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            humanScore++
+        } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+            computerScore++
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {            
+            humanScore++
+        } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            computerScore++
+        } else {
+            prompt(`Invalid input! Refresh the page!!`)
+        }
+        console.log(`Human: ${humanScore} Computer: ${computerScore}`)
+
+        if (humanScore === 3 || computerScore === 3) {
+            break
+        }
     }
-}
-
-function getHumanChoice(){
-    let humanChoice = prompt('What will you play?')
-    const choice1 = 'rock'
-    const choice2 = 'paper'
-    const choice3 = 'scissor'
-
-    if (humanChoice.toUpperCase() == choice1.toUpperCase()) {
-        return 'Human played: ' + humanChoice.toUpperCase()
-    } else if (humanChoice.toUpperCase() == choice2.toUpperCase()) {
-        return 'Human played: ' + humanChoice.toUpperCase()
-    } else if (humanChoice.toUpperCase() == choice3.toUpperCase()) {
-        return 'Human played: ' + humanChoice.toUpperCase()
+    let gameResult = `Final Result: `
+    if (humanScore > computerScore ) {
+        gameResult += 'Human Wins!'
+    } else if (computerScore > humanScore ) {
+        gameResult += 'Computer Wins!'
     } else {
-        return 'Invalid choice! Please chooose between rock, paper or scissor.'
+        gameResult += `It's a tie!`
     }
+    console.log(gameResult)
 }
-console.log(getHumanChoice())
-console.log(getComputerChoice())
+playGame()
